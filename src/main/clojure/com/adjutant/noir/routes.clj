88eb61c7461b12325925df9029059c0fc05d75b7
Jason Rose <jason.rose@meltmedia.com>
2011-12-11 10:44:58 -0700

    Generating some json and using noir to generate urls for me

--- src/main/clojure/com/adjutant/noir/routes.clj
+++ src/main/clojure/com/adjutant/noir/routes.clj
@@ -1,5 +1,26 @@
 (ns com.adjutant.noir.routes
-  (:use noir.core))
+  (:use noir.core)
+  (:require [noir.response :as response]
+            [noir.request :as request]
+            [clojure.string :as string]))
 
-(defpage "/hello" []
+(defn full-url-for
+  ([uri]
+;    (.. System err (println (str (request/ring-request))))
+    (str
+      (string/replace (str ((request/ring-request) :scheme)) #":" "")
+      "://"
+      ((:headers (request/ring-request)) "host")
+      uri)))
+
+(defpage hello "/hello" []
   "hello")
+
+(defpage root "/" []
+  (response/json {:urls [
+                         {:href (full-url-for (url-for hello))
+                          :rel "test resource"
+                          :type "text/plain"}
+                         {:href (full-url-for (url-for root))
+                          :rel "self"
+                          :type "application/json"}]}))
