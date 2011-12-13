031cefe20032ba9a19a2ee4f5bf979ef68560c24
Jason Rose <jason.rose@meltmedia.com>
2011-12-12 19:44:03 -0700

    Adding lein support

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject adjutant "1.0.0-SNAPSHOT"
+  :description "Adjutant is a tool that monitors servers and allows for actions to be taken upon them."
+  :url "http://github.com/jasonrose/adjutant"
+  :source-path "src/main/clojure"
+  :compile-path "target/classes"
+  :resources-path "src/main/resources"
+  :test-path "src/test/clojure"
+  :library-path "target/lib"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 ])
