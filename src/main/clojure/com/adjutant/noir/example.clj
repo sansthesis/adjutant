4ea1c01a4f2f3f93acd3fff068855e146ac9a867
Jason Rose <jason.rose@meltmedia.com>
2012-02-18 14:18:46 -0700

    Removing old project to start over

--- src/main/clojure/com/adjutant/noir/example.clj
+++
@@ -1,8 +0,0 @@
-(ns com.adjutant.noir.example
-  (:use noir.core)
-  (:require [noir.response :as response]
-            [com.adjutant.noir.common :as common])) 
-
-;; This is a test page so I can see if it works or not.
-(defpage hello "/hello"
-  [] (response/content-type "text/html" (str "hello from " (common/full-url-for (url-for hello)))))
