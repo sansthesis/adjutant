859e26b97ca0401aee1cbf4c31be01dc3e79d6c6
Jason Rose <jason.rose@meltmedia.com>
2012-02-18 14:36:06 -0700

    Reinitalizing the app

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :resources-path "src/main/resources"
   :test-path "src/test/clojure"
   :library-path "target/lib"
-  :main "com.adjutant.noir.server"
+  :main adjutant.server
+  :repl-init adjutant.server
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.1"]])
---
+++ src/main/clojure/adjutant/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns adjutant.views.welcome
+  (:require [adjutant.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to adjutant"]))
